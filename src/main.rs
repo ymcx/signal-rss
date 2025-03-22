abfce87803cdff22e05db1c36c84e9bcdf1b34e1
Tino <89810988+ymcx@users.noreply.github.com>
2025-03-22 23:10:56 +0200

    better error handling, url modifying

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,18 @@ use dbus::blocking::LocalConnection;
 use rss::Channel;
 use std::{env::args, error::Error, fs, thread::sleep, time::Duration};
 
+fn modify_url(url: &str, description: &str) -> String {
+    if ["www.memeorandum.com", "www.techmeme.com"]
+        .iter()
+        .any(|&i| url.contains(i))
+    {
+        let parts: Vec<&str> = description.split('"').collect();
+        return parts[1].to_string();
+    }
+
+    url.replace("www.youtube.com", "yewtu.be")
+}
+
 async fn parse_feeds(feeds: &str, checked: &DateTime<Utc>) -> Result<Vec<String>, Box<dyn Error>> {
     let mut messages: Vec<String> = Vec::new();
 
@@ -18,30 +30,41 @@ async fn parse_feeds(feeds: &str, checked: &DateTime<Utc>) -> Result<Vec<String>
 
         match format {
             "atom" => {
-                let feed = Feed::read_from(content)?;
-                for entry in feed.entries {
-                    let time = entry.published.unwrap();
+                let feed = Feed::read_from(content);
+                if let Err(_) = feed {
+                    continue;
+                }
+
+                for entry in feed.unwrap().entries {
+                    let time = entry.published.unwrap_or_default();
                     if &time <= checked {
                         continue;
                     }
 
                     let title = entry.title.value;
-                    let link = entry.links[0].href.clone();
+                    let description = entry.summary.unwrap_or_default().value;
+                    let link = modify_url(&entry.links[0].href, &description);
                     let message = format!("{}\n{}", title, link);
 
                     messages.push(message);
                 }
             }
             "rss" => {
-                let feed = Channel::read_from(content)?;
-                for entry in feed.items {
-                    let time = DateTime::parse_from_rfc2822(&entry.pub_date.unwrap())?;
+                let feed = Channel::read_from(content);
+                if let Err(_) = feed {
+                    continue;
+                }
+
+                for entry in feed.unwrap().items {
+                    let time = DateTime::parse_from_rfc2822(&entry.pub_date.unwrap_or_default())
+                        .unwrap_or_default();
                     if &time <= checked {
                         continue;
                     }
 
-                    let title = entry.title.unwrap();
-                    let link = entry.link.unwrap();
+                    let title = entry.title.unwrap_or_default();
+                    let description = entry.description.unwrap_or_default();
+                    let link = modify_url(&entry.link.unwrap_or_default(), &description);
                     let message = format!("{}\n{}", title, link);
 
                     messages.push(message);
